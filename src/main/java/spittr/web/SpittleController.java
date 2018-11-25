70a2a1c4adf866df3e6f145ad2b92608abdf8407
minhnln <mingngo158@gmail.com>
2018-11-25 19:15:16 +1000

    Implement method using query parameters

--- src/main/java/spittr/web/SpittleController.java
+++ src/main/java/spittr/web/SpittleController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import spittr.Spittle;
 import spittr.data.SpittleRepository;
 
@@ -25,13 +26,13 @@ public class SpittleController {
     // Model is a map
     // The default key is inferred from the type of object
     // For example: List<Spittle> -> spittleList
-    @RequestMapping(method = RequestMethod.GET)
-    public String spittles(Model model) {
-        model.addAttribute(
-                spittleRepository.findRecentSpittles(20)
-        );
-        return "spittles";
-    }
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String spittles(Model model) {
+//        model.addAttribute(
+//                spittleRepository.findRecentSpittles(20)
+//        );
+//        return "spittles";
+//    }
 
     // Another way
     // Logical view name is inferred from the request path
@@ -40,5 +41,13 @@ public class SpittleController {
 //    public List<Spittle> spittles() {
 //        return spittleRepository.findRecentSpittles(20);
 //    }
+
+    // Using query parameters
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Spittle> spittles(
+            @RequestParam("max") long max,
+            @RequestParam("count") int count) {
+        return spittleRepository.findSpittles(max, count);
+    }
 }
 
